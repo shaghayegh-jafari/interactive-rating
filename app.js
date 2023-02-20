0718b039f9488400203de88cac979c5b67f875e7
Shaghayegh-jg <123248281+Shaghayegh-jg@users.noreply.github.com>
2023-02-20 15:30:17 +0330

    add file

---
+++ app.js
@@ -0,0 +1,31 @@
+function myFunction() {
+  document.getElementById("card-page").style.display = "none";
+  document.querySelector(".page").style.display = "block";
+}
+
+function chBackcolor() {
+  let d = document.body;
+  d.classList.toggle("light-mode");
+}
+const rates = document.querySelectorAll(".number");
+const rating = document.querySelector(".rating");
+
+
+number.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = number.innerHTML;
+  });
+});
+
+// let s = document.querySelectorAll(".numbers a");
+// s((btn, i),()=> {
+//   btn.addEventListener("click", () => {
+//     document.getElementsByClassName(i + 1);
+//   });
+// });
+// function addClassA(e){
+//   elem = document.querySelectorAll(".active");
+//   elem.classList.remove('active');
+//   e.addClass("active");
+//   console.log(e);
+// }
